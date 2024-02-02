29d67646347c6613b8ec7ad363b6c482f8007ddc
Cupid Valentine <westerngod@126.com>
2024-02-02 23:24:18 +0800

    chore: update eslint conf

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,16 @@
-import antfu from '@antfu/eslint-config'
+// import antfu from '@antfu/eslint-config'
 
-export default antfu({
+// export default antfu({
+//   react: true,
+//   rules: {
+//     curly: ['off'],
+//   },
+// })
+
+// eslint.config.js
+const antfu = require('@antfu/eslint-config').default
+
+module.exports = antfu({
   react: true,
   rules: {
     curly: ['off'],
