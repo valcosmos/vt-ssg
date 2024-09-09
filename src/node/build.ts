b0803f945ae154afc826d0a091e69e6b6bb3319e
Cupid Valentine <westerngod@126.com>
2024-09-09 23:57:29 +0800

    feat: add ssg

---
+++ src/node/build.ts
@@ -0,0 +1,79 @@
+import { rm, writeFile } from 'node:fs/promises'
+import { join, resolve } from 'node:path'
+import process from 'node:process'
+import { build as viteBuild } from 'vite'
+import type { RollupOutput } from 'rollup'
+import type { InlineConfig } from 'vite'
+import { CLIENT_ENTRY_PATH, SERVER_ENTRY_PATH } from './constants'
+
+export async function bundle(root: string) {
+  try {
+    const resolveViteConfig = (isServer: boolean): InlineConfig => {
+      return {
+        mode: 'production',
+        root,
+        build: {
+          ssr: isServer,
+          outDir: isServer ? '.temp' : 'build',
+          rollupOptions: {
+            input: isServer ? SERVER_ENTRY_PATH : CLIENT_ENTRY_PATH,
+            output: {
+              format: isServer ? 'cjs' : 'esm',
+            },
+          },
+        },
+      }
+    }
+
+    const clientBuild = async () => {
+      return viteBuild(resolveViteConfig(false))
+    }
+
+    const serverBuild = async () => {
+      return viteBuild(resolveViteConfig(true))
+    }
+
+    const [clientBundle, serverBundle] = await Promise.all([
+      clientBuild(),
+      serverBuild(),
+    ])
+
+    return [clientBundle, serverBundle]
+  }
+  catch (error) {
+    throw new Error(error as string)
+  }
+}
+
+export async function renderPage(render: () => string, root: string, clientBundle?: RollupOutput) {
+  const appHtml = render()
+  const clientChunk = clientBundle?.output.find(chunk => chunk.type === 'chunk' && chunk.isEntry)
+
+  const html = `
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+      </head>
+      <body>
+        <div id="root">${appHtml}</div>
+        <script src="/${clientChunk?.fileName}" type="module" />
+      </body>
+    </html>
+  `.trim()
+
+  await writeFile(join(root, 'build', 'index.html'), html, 'utf-8')
+  await rm(join(root, '.temp'), { recursive: true })
+}
+
+export async function build(root: string) {
+  const [clientBundle, _serverBundle] = await bundle(root)
+
+  const serverEntryPath = resolve(process.cwd(), 'docs', '.temp', 'ssr-entry.js')
+
+  // eslint-disable-next-line ts/no-require-imports
+  const { render } = require(serverEntryPath)
+  await renderPage(render, root, clientBundle as RollupOutput)
+}
--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,4 +1,5 @@
 import cac from 'cac'
+import { build } from './build'
 import { createDevServer } from './dev'
 
 const cli = cac('vt-ssg').version('0.0.1').help()
@@ -7,6 +8,10 @@ cli.command('dev [root]', 'start dev server').action(async (root: string) => {
   const server = await createDevServer(root)
   await server.listen()
   server.printUrls()
+})
+
+cli.command('build [root]', 'build in production').action(async (root: string) => {
+  await build(root)
 })
 
 cli.parse()
