95c4df2e8b84e040c53f9d012f74142470b8578a
Cupid Valentine <westerngod@126.com>
2024-09-15 23:47:19 +0800

    feat: add hmr for md

--- src/node/plugin-mdx/index.ts
+++ src/node/plugin-mdx/index.ts
@@ -1,6 +1,7 @@
 import type { Plugin } from 'vite'
+import { pluginMdxHmr } from './pluginMdxHmr'
 import { pluginMdxRollup } from './pluginMdxRollup'
 
 export async function pluginMdx(): Promise<Plugin[]> {
-  return [await pluginMdxRollup()]
+  return [await pluginMdxRollup(), pluginMdxHmr()]
 }
